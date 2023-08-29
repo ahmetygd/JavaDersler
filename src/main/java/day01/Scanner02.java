cbc532a84cd09ff5a23825c4b73ce73760232a9f
Ahmet Yiğid <ahmetalperenygd@gmail.com>
2023-08-29 10:04:48 +0300

    day01-02

---
+++ src/main/java/day01/Scanner02.java
@@ -0,0 +1,26 @@
+package day01;
+
+import java.util.Scanner;
+
+public class Scanner02 {
+
+    public static void main(String[] args) {
+        //Kullanicidan iki sayi alip 4 islem yapan ve islemlerin sonuclarini ekrana yazdiran kodu yaziniz
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Lütfen iki sayı giriniz");
+        double number1 = input.nextDouble();
+        double number2 = input.nextDouble();
+
+        System.out.println("Toplama= "+ (number1+number2));
+        System.out.println("Çıkarma= "+ (number1-number2));
+        System.out.println("Çarpma= "+ (number1*number2));
+        System.out.println("Bölme= "+ (number1/number2));
+
+
+
+
+
+    }
+
+}
