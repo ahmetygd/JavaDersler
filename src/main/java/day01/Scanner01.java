b35675d1aae4d903f7dce47c7fd535049221b169
Ahmet Yiğid <ahmetalperenygd@gmail.com>
2023-08-29 09:32:39 +0300

    day01

---
+++ src/main/java/day01/Scanner01.java
@@ -0,0 +1,41 @@
+package day01;
+
+import java.util.Scanner;
+
+public class Scanner01 {
+    public static void main(String[] args) {
+/*
+       Ornek:  Kullanıcıdan
+       i)Adini ve Soyadini
+       ii)Yaşını
+       iii)Boyunu
+       iv)kilosunu
+       v) medeni durumunu girmesini isteyin
+       Ardından bunları bir etiketle konsolda farklı satırlarda
+       yazdırın
+        */
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen Adınızı Ve Soyadınızı Giriniz");
+        String nameSurname = input.nextLine();
+
+        System.out.println("Lütfen Yaşınızı Giriniz");
+        int age = input.nextInt();
+
+        System.out.println("Lütfen Boyunuzu Giriniz");
+        double height = input.nextDouble();
+
+        System.out.println("Lütfen Kilonuzu Giriniz");
+        int weight = input.nextInt();
+
+        System.out.println("Lütfen Medeni Durumunuzu Giriniz");
+        String marriage = input.next();
+
+        System.out.println("nameSurname = " + nameSurname);
+        System.out.println("age = " + age);
+        System.out.println("height = " + height);
+        System.out.println("weight = " + weight);
+        System.out.println("marriage = " + marriage);
+
+    }
+}
